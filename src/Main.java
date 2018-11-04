b836d2f121cdbef2a11584806821d6bbfbd5fdd3
achmadsatyap <satyapradana98@gmail.com>
2018-11-04 22:23:31 +0700

    class Main

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author RADEON
+ */
+public class Main {
+    public static void main(String[] args) {
+        Service servis = new Service("servis hp", 25000);
+        Hp hp = new Hp("Android", 20000, "Samsung");
+        Pegawai pegawai = new Pegawai("Agus", "CS");
+        Transaksi transaksi = new Transaksi(hp, servis, pegawai);
+        
+        transaksi.print();
+    }
+}
