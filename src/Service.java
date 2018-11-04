4d8d81a8c7ffb1ac0d1e65edc0562e1b4d35a9a4
achmadsatyap <satyapradana98@gmail.com>
2018-11-04 22:09:19 +0700

    deklarasikan layanan, harga, membuat method, dan konstruktor

---
+++ src/Service.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author RADEON
+ */
+public class Service {
+    private String layanan;
+    private int harga;
+
+    public Service(String layanan, int harga) {
+        this.layanan = layanan;
+        this.harga = harga;
+    }
+    
+    public int getHarga(){
+        return harga;
+    }
+    public String info(){
+        String info = "";
+        info += "Layanan yang diberikan : " + this.layanan + "\n";
+        info += "Harga : " + this.harga + "\n";
+        return info;
+    }
+}
