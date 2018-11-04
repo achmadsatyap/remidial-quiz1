2c81e7771f233725a23665f2f5ac5dd19f8f08dd
achmadsatyap <satyapradana98@gmail.com>
2018-11-04 22:29:29 +0700

    edit

--- src/Main.java
+++ src/Main.java
@@ -10,11 +10,11 @@
  */
 public class Main {
     public static void main(String[] args) {
-        Service servis = new Service("servis hp", 25000);
+        Service s = new Service("servis hp", 25000);
         Hp hp = new Hp("Android", 20000, "Samsung");
-        Pegawai pegawai = new Pegawai("Agus", "CS");
-        Transaksi transaksi = new Transaksi(hp, servis, pegawai);
+        Pegawai p = new Pegawai("Agus", "CS");
+        Transaksi t = new Transaksi(hp, s, p);
         
-        transaksi.print();
+        t.print();
     }
 }
