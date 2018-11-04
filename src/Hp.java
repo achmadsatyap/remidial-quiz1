ece53fa7def573b5c9357bff0ba97465ce59ac10
achmadsatyap <satyapradana98@gmail.com>
2018-11-04 21:44:51 +0700

    deklarasikan jenis, merk, harga, membuat method, dan konstruktor

--- src/Hp.java
+++ src/Hp.java
@@ -9,5 +9,24 @@
  * @author RADEON
  */
 public class Hp {
+    private String jenis;
+    private int harga;
+    private String merk;
+
+    public Hp(String jenis, int harga, String merk) {
+        this.jenis = jenis;
+        this.harga = harga;
+        this.merk = merk;
+    }
     
+    public int getHarga(){
+        return harga;
+    }
+    public String info(){
+        String info = "";
+        info += "Jenis Hp : " + this.jenis + "\n";
+        info += "Merk Hp : " + this.merk + "\n";
+        info += "Harga : " + this.harga + "\n";
+        return info;
+    }
 }
